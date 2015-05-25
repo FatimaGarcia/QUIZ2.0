9fd160c67cd539fb99d03455c24897c690f10b93
Fatima Garcia <f.garciac@alumnos.upm.es>
2015-05-26 00:10:17 +0200

    Autenticacion de usuarios

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require ('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -15,6 +16,11 @@ router.get('/author', function(req, res) {
 	res.render('author', { autor: 'Fatima Garcia', errors: []});
 });
 
+/*GET LOGIN/LOGOUT*/
+router.get('/login', sessionController.new); // formulario login
+router.post('/login', sessionController.create); // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
+
 /*GET PREGUNTAS / RESPUESTAS*/
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
@@ -26,6 +32,7 @@ router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 router.post('/quizes/create', quizController.create);
 router.put('/quizes/:quizId(\\d+)', quizController.update);
 
+/*GET /quizes/comments*/
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
 module.exports = router;
